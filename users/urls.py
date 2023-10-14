c7b30151a3bf573ac34345ee5f5283bbcd682c33
ibragim-Nalgiev <ibragim.nalgieff@yandex.ru>
2023-10-14 22:36:11 +0300

    made all Crud for app users

---
+++ users/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from users.apps import UsersConfig
+from users import views
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('users/', views.UserListAPIView.as_view(), name='list_users'),
+    path('users/detail/<int:pk>/', views.UserRetrieveAPIView.as_view(), name='detail_users'),
+    path('users/create/', views.UserCreateAPIView.as_view(), name='create_users'),
+    path('users/update/<int:pk>/', views.UserUpdateAPIView.as_view(), name='update_users'),
+    path('users/delete/<int:pk>/', views.UserDestroyAPIView.as_view(), name='delete_users'),
+]
