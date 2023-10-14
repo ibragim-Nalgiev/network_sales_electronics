46554c286030b3b6c9d2f459cbbf6e5bb201e13a
ibragim-Nalgiev <ibragim.nalgieff@yandex.ru>
2023-10-14 22:29:37 +0300

    created model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from users.manager import UserManager
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    """Класс для отображения пользователей"""
+
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='почта')
+    phone = models.CharField(max_length=30, verbose_name='телефон', **NULLABLE)
+    country = models.CharField(max_length=235, verbose_name='страна', **NULLABLE)
+    city = models.CharField(max_length=235, verbose_name='город', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+
+    def __str__(self):
+        return self.email
