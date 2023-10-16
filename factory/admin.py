ee6e738da31250e6a6d2e03c3824b5b66f90ba88
ibragim-Nalgiev <ibragim.nalgieff@yandex.ru>
2023-10-16 22:34:20 +0300

    created app factory with  model Factory wrote full CRUD

---
+++ factory/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Factory
+from contacts.services import ContactCityFilter
+
+
+@admin.register(Factory)
+class FactoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'main_network', 'contact', 'created_at', 'is_active',)
+    list_filter = ('name', ContactCityFilter, 'is_active',)
+    search_fields = ('name',)
+    
